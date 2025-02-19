72dcbd85ebb7129257014ae682664883cd83f05a
ARTHUR DE ANDRADE SANTANA <162452096+caiua2669@users.noreply.github.com>
2025-02-19 14:09:10 +0000

    [feat]-first commit

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,12 @@
+const paymentController= {
+    async createPayment(req,res) {
+        try {
+            res.status(201).json({message:"Payment create sucessfuly"});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: error.message});
+        }
+
+    }
+}
+export default paymentController;
--- src/server.js
+++ src/server.js
@@ -2,10 +2,28 @@
 import express from 'express';
 const server = express();
 
+import routerPayment from './routes/payment.routes.js';
+
+
+server.use(express.json());
+server.use("/api",routerPayment);
+
 const PORT = process.env.PORT || 3000;
 
 server.get("/",(req,res)=>{
-    res.send("Hello World " + new Date());
+    res.send("GET " + new Date());
+});
+
+server.post("/",(req,res)=>{
+    res.send("POST" + new Date());
+});
+
+server.delete("/",(req,res)=>{
+    res.send("DELETE " + new Date());
+});
+
+server.patch("/",(req,res)=>{
+    res.send("PATCH " + new Date());
 });
 
 server.listen(PORT,()=>{
